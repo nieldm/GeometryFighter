874e19d3128bdf269b837b0d3b2c6879993990f0
Daniel Mendez <daniel.mendez@grability.com>
2016-10-05 17:41:13 -0500

    Amend

--- GeometryFighter/GameViewController.swift
+++ GeometryFighter/GameViewController.swift
@@ -5,20 +5,79 @@ class GameViewController: UIViewController {
     
     var scnView: SCNView!
     var scnScene: SCNScene!
+    var cameraNode: SCNNode!
+    var spawnTime: TimeInterval = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setupView()
         setupScene()
+        setupCamera()
     }
     
     //MARK: Setup
     func setupView() {
         scnView = self.view as! SCNView
+        
+        scnView.showsStatistics = true
+        scnView.allowsCameraControl = true
+        scnView.autoenablesDefaultLighting = true
+        
+        scnView.delegate = self
+        scnView.play(nil)
     }
     
     func setupScene() {
         scnScene = SCNScene()
         scnView.scene = scnScene
+        scnScene.background.contents = "GeometryFighter/GeometryFighter.scnassets/Textures/Background_Diffuse.png"
+    }
+    
+    func setupCamera() {
+        cameraNode = SCNNode()
+        cameraNode.camera = SCNCamera()
+        cameraNode.position = SCNVector3(x: 0, y: 5, z: 10)
+        scnScene.rootNode.addChildNode(cameraNode)
+    }
+    
+    //MARK: Spawns
+    func spawnShape() {
+        var geometry:SCNGeometry
+
+        switch ShapeType.random() {
+        default:
+            geometry = SCNBox(width: 1.0, height: 1.0, length: 1.0,
+                              chamferRadius: 0.0)
+        }
+
+        geometry.materials.first?.diffuse.contents = UIColor.random()
+        let geometryNode = SCNNode(geometry: geometry)
+        geometryNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
+        
+        let randomX = Float.random(-2, max: 2)
+        let randomY = Float.random(10, max: 18)
+        let force = SCNVector3(x: randomX, y: randomY , z: 0)
+        let position = SCNVector3(x: 0.05, y: 0.05, z: 0.05)
+        geometryNode.physicsBody?.applyForce(force, at: position,
+                                             asImpulse: true)
+
+        scnScene.rootNode.addChildNode(geometryNode)
+    }
+    
+    func cleanScene() {
+        for node in scnScene.rootNode.childNodes {
+            if node.presentation.position.y < -2 {
+                node.removeFromParentNode()
+            }
+        } }
+}
+
+extension GameViewController: SCNSceneRendererDelegate {
+    func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        cleanScene()
+        if time > spawnTime {
+            spawnShape()
+            spawnTime = time + TimeInterval(Float.random(0.2, max: 1.5))
+        }
     }
 }
